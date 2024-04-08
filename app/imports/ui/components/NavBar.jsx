05a7aa13b9919a87a03633f9ce93f0dd970ab218
Ronan Russel Andal <rrandal@hawaii.edu>
2024-04-07 20:45:14 -1000

    commit 2

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -15,28 +15,43 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          <h4><Image roundedCircle src="/images/campus-center.png" width="50px" /> Rec Center Warriors</h4>
+        <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
+          <Image roundedCircle src="/images/campus-center.png" width="50px" />
+          <span className="ms-2" style={{ fontWeight: 'bold' }}>Rec Center Warriors</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
-            {currentUser ? ([
-              <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Find A Buddy</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">Exercises</Nav.Link>,
-            ]) : ([
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">Exercises</Nav.Link>,
-            ])}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
-            ) : ''}
+            {currentUser ? (
+              [
+                <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Find A Buddy</Nav.Link>,
+                <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">Exercises</Nav.Link>,
+              ]
+            ) : (
+              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list">Exercises</Nav.Link>
+            )}
+            {Roles.userIsInRole(Meteor.userId(), 'admin') && (
+              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin">Admin</Nav.Link>
+            )}
           </Nav>
           <Nav className="justify-content-end">
             {/* New navbar item for logged-in users */}
             {currentUser !== '' && (
-              <Nav.Link as={NavLink} to="/workout-sched">
-                Workout Schedule
-              </Nav.Link>
+              <>
+                <Nav.Link as={NavLink} to="/workout-sched">
+                  Workout Schedule
+                </Nav.Link>
+                <NavDropdown id="navbar-current-user" title={currentUser}>
+                  <NavDropdown.Item id="navbar-view-profile" as={NavLink} to="/viewprofile">
+                    <PersonSquare />
+                    View Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
+                    <BoxArrowRight />
+                    Sign out
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
             )}
 
             {/* Dropdown for login/logout */}
@@ -52,19 +67,9 @@ const NavBar = () => {
                 </NavDropdown.Item>
               </NavDropdown>
             ) : (
-              <NavDropdown id="navbar-current-user" title={currentUser}>
-                <NavDropdown.Item id="navbar-view-profile" as={NavLink} to="/viewprofile">
-                  <PersonSquare />
-                  View Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
-                  <BoxArrowRight />
-                  Sign out
-                </NavDropdown.Item>
-              </NavDropdown>
+              null // No action needed here since it's handled in the logged-in condition above
             )}
           </Nav>
-
         </Navbar.Collapse>
       </Container>
     </Navbar>
