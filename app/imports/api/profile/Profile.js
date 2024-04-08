f99e92812e6dad8f3f9dad55000ab36601549679
HunterVT <hvt7@hawaii.edu>
2024-04-07 20:58:37 -1000

    add profiles collection and default profile data

---
+++ app/imports/api/profile/Profile.js
@@ -0,0 +1,39 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+class ProfilesCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ProfilesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      firstName: String,
+      lastName: String,
+      year: {
+        type: String,
+        allowedValues: ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate', 'Faculty'],
+        defaultValue: 'Freshman',
+      },
+      major: String,
+      email: String,
+      interests: {
+        type: String,
+        allowedValues: ['General Health/Fitness', 'Bodybuilding/Aesthetics', 'Powerlifting', 'Crossfit', 'Other'],
+        defaultValue: 'General Health/Fitness',
+      },
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StuffsCollection.
+ * @type {StuffsCollection}
+ */
+export const Profiles = new ProfilesCollection();
--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Profiles } from '../../api/profile/Profile.js';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,23 @@ const addData = (data) => {
   Stuffs.collection.insert(data);
 };
 
+const addProfile = (profile) => {
+  console.log(`  Adding: ${profile.lastName} (${profile.email})`);
+  Profiles.collection.insert(profile);
+};
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.forEach(data => addData(data));
+  }
+}
+
+// Initialize the StuffsCollection if empty.
+if (Profiles.collection.find().count() === 0) {
+  if (Meteor.settings.defaultProfiles) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultProfiles.forEach(profile => addProfile(profile));
   }
 }
