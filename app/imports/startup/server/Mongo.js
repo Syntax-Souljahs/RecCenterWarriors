6377e372a9e9c9a7e45497644b44cf8c5a5a83f8
Isai Foglesong <isai.mexina@gmail.com>
2024-04-11 12:49:48 -1000

    Rid of SutffCollection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,27 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Profiles } from '../../api/profile/Profile.js';
 
 /* eslint-disable no-console */
-
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
 
 const addProfile = (profile) => {
   console.log(`  Adding: ${profile.lastName} (${profile.email})`);
   Profiles.collection.insert(profile);
 };
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 
 // Initialize the ProfileCollection if empty.
 if (Profiles.collection.find().count() === 0) {
