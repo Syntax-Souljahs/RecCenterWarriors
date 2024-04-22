3ecb3eb4903b92d8cf8d8331e071d760002f25b0
UHM-ShaneB <156708366+UHM-ShaneB@users.noreply.github.com>
2024-04-21 15:33:03 -1000

    Added Buddy Up page to Navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -26,6 +26,7 @@ const NavBar = () => {
               [
                 <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Find A Buddy</Nav.Link>,
                 <Nav.Link id="list-stuff-nav" as={NavLink} to="/exercises" key="exercises">Exercises</Nav.Link>,
+                <Nav.Link id="buddy-up-nav" as={NavLink} to="/buddy-up" key="buddy-up">Buddy Up</Nav.Link>,
               ]
             ) : (
               [
