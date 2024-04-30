8d04d6891bd63932f9bc01a15850e49ba5dce310
HunterVT <hvt7@hawaii.edu>
2024-04-29 22:21:16 -1000

    Create Favorites Collection for use in favorites page

---
+++ app/imports/api/favorites/Favorites.js
@@ -0,0 +1,34 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+class FavoritesCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ExercisesCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      exerciseId: String,
+      owner: String,
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+  }
+
+  // Method to add a new exercise to the collection
+  addFavorite(exerciseId, owner) {
+    return this.collection.insert({ exerciseId, owner });
+  }
+
+  // Method to remove an exercise from the collection
+  removeFavorite(exerciseId) {
+    this.collection.remove({ _id: exerciseId });
+  }
+}
+
+/**
+ * The singleton instance of the ExercisesCollection.
+ * @type {ExercisesCollection}
+ */
+export const Favorites = new FavoritesCollection();
