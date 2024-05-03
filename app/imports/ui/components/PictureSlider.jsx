919cde0109561d2ae256cef1e82d85a41c29ad18
Ronan Russel Andal <rrandal@hawaii.edu>
2024-05-02 21:01:22 -1000

    slider

--- app/imports/ui/components/PictureSlider.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-<section className="container">
-  <div className="slider-wrapper">
-
-  </div>
-</section>;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -37,68 +37,74 @@ const Landing = () => {
         </Row>
       ) : (
         <Row className="align-middle text-center">
-          <Col xs={2}/>
+          <Col xs={2} />
           <Col xs={8}>
-            <h1>Welcome back {currentUser}</h1>
+            <h1 style={{ paddingBottom: '25px' }}>Welcome back {currentUser}</h1>
           </Col>
 
           <section className="container">
             <div className="slider-wrapper">
               <div className="slider">
-                <img id="slide-1" src="/images/exercises.jpeg" alt="exercises pic"/>
-                <img id="slide-2" src="/images/buddy-up.jpeg" alt="Buddy Up pic"/>
-                <img id="slide-3" src="/images/guide.jpeg" alt="Guide pic"/>
-                <img id="slide-4" src="/images/workout-schedule.jpeg" alt="Workout Schedule pic"/>
+                <img id="slide-1" src="/images/forslides1.jpeg" alt="exercises pic" />
+                <img id="slide-2" src="/images/forslides2.jpeg" alt="Buddy Up pic" />
+                <img id="slide-3" src="/images/forslides3.jpeg" alt="Guide pic" />
+                <img id="slide-4" src="/images/forslides4.jpeg" alt="Workout Schedule pic" />
                 <div className="slider-nav">
-                  <a href="#slide-1"></a>
-                  <a href="#slide-2"></a>
-                  <a href="#slide-3"></a>
-                  <a href="#slide-4"></a>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label */}
+                  <a href="#slide-1" />
+                  {/* eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label */}
+                  <a href="#slide-2" />
+                  {/* eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label */}
+                  <a href="#slide-3" />
+                  {/* eslint-disable-next-line jsx-a11y/anchor-has-content,jsx-a11y/control-has-associated-label */}
+                  <a href="#slide-4" />
                 </div>
               </div>
             </div>
           </section>
           <row className="justify-content-center">
-            <div style={{ color: '#ffff' }}><h2>Plan your Workouts</h2>
+            <div style={{ color: '#ffff', paddingTop: '50px' }}><h2>Join the community!</h2>
               <p>Join our vibrant gym community and be part of something bigger.</p>
-              <p>Our workout schedule not only helps you stay organized but also provides opportunities to connect with fellow members,</p> share fitness tips, and motivate each other on your fitness journey.</div>
-            <div className="landing-buttons">
-              <a href = "/workout-sched">
+              <p>Our workout schedule not only helps you stay organized but also provides opportunities to connect with fellow members,</p> share fitness tips, and motivate each other on your fitness journey.
+            </div>
+            <div className="landing-buttons" style={{ paddingTop: '25px' }}>
+              <a href="/workout-sched">
+                {/* eslint-disable-next-line react/button-has-type */}
                 <button>Workout Schedule</button>
               </a>
             </div>
           </row>
           <Row className="align-items-center mt-4">
             <Col xs={7}>
-              <img src="/images/exercises.jpeg" alt="Exercises" className="img-fluid"/>
+              <img src="/images/exercises.jpeg" alt="Exercises" className="img-fluid" />
             </Col>
             <Col xs={5}>
               <div style={{ color: '#ffff' }}>
                 <h2>Recreation Center</h2>
-                <p>Whether you're looking to build muscle, improve endurance, or simply maintain your fitness routine, our Exercises page has something for everyone.</p>
+                <div style={{ color: 'lightgray' }}><p>Whether you&apos;re looking to build muscle, improve endurance, or simply maintain your fitness routine, our Exercises page has something for everyone.</p></div>
               </div>
               <div className="landing-buttons mt-4">
                 <a href="/exercises">
+                  {/* eslint-disable-next-line react/button-has-type */}
                   <button>Exercises</button>
                 </a>
               </div>
             </Col>
           </Row>
-
-
           <Col xs={6} className="mt-4">
             <Row className="align-middle text-center"><h2 style={{ color: '#ffff' }}>Find a Workout Partner</h2>
-              <div style={{ color: 'lightgray' }}> Whether you're looking to build muscle, improve endurance, or simply maintain your fitness routine, our Exercises page has something for everyone.</div>
+              <div style={{ color: 'lightgray' }}> Whether you&apos;re looking to build muscle, improve endurance, or simply maintain your fitness routine, our Exercises page has something for everyone.</div>
               {/* eslint-disable-next-line react/button-has-type */}
               <div className="landing-buttons mt-4">
                 <a href="/buddy-up">
+                  {/* eslint-disable-next-line react/button-has-type */}
                   <button>Buddy up</button>
                 </a>
               </div>
             </Row>
           </Col>
           <Col xs={6} className="mt-4">
-            <img src="/images/buddy-up.jpeg" alt="Exercises" className="img-fluid"/>
+            <img src="/images/buddy-up.jpeg" alt="Exercises" className="img-fluid" />
           </Col>
         </Row>
       )}
